1b74082ccb1092354e2aa3e4b69fe5d06cfe9aed
ribpy <mdriazbappy22@gmail.com>
2023-01-26 23:52:19 +0600

    source code

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-const XLSX=require("xlsx")
-const file=XLSX.readFile("Book1.xlsx")
+const XLSX =require('xlsx');
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const clc = require("cli-color");
+
+
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const today=new Date().getDay()
-const fileSheet=file.Sheets[weekday[today]]
-const arrData=XLSX.utils.sheet_to_json(fileSheet)
-let keywords=arrData.map(el=>el.Keyword)
-console.log(keywords[0]);
+const today=new Date().getDay()// get the current day (0 for Sunday, 1 for Monday, etc.)
+let sheet=weekday[today]; //Select the sheet according current day
 
-const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
-  try {
-    //assuming you have an array of keywords
-    //iterating through all keywords
-    for (let keyword of keywords){
-        await driver.get('http://www.google.com');
-        await driver.findElement(By.name('q')).sendKeys(keyword);
+
+// Now we can access the data in the sheet for the current day
+// We can use the SheetJS library to parse the Excel file and access the data in the sheet
+
+// Read the Excel file
+let workbook = XLSX.readFile("Book1.xlsx");
+let worksheet = workbook.Sheets[sheet];
+let json = XLSX.utils.sheet_to_json(worksheet);
+let data=json.map(e=>e.Keyword)
+console.log(data[0])
+
+
+async function run(){
+     try {
+      const browser = await puppeteer.launch({headless:false}); 
+    const page = await browser.newPage();
+    for (let i= 0; i< data.length; i++){
+        const keyword = data[i];
+        await page.goto('https://www.google.com'); 
+        await page.focus('input[name="q"]');
+        await page.keyboard.type(keyword); 
+
+        await page.waitForSelector('[role="listbox"]');
+        await new Promise(resolve => setTimeout(resolve, 2000)); 
+        const html = await page.evaluate(() => document.body.innerHTML);
+        const $ =  cheerio.load(html);
+        const options = $('[role="listbox"] [role="option"]').map((i, el) =>$(el).text()).get();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        options.sort((a, b) => a.length - b.length);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const shortest = options.shift();
+        const longest = options.pop();
         
-        let titles =await driver.findElements(By.className("wM6W7d"));
-        let title = titles.map( async title =>await title.getText());
-        console.log("title:"+title.entries());
-        await driver.wait(until.titleIs(keyword + '- Google Search'), 2000);
+        console.log(clc.blue(`Shortest Option for ${keyword} -> `,shortest));
+        console.log(clc.green(`Longest  Option for ${keyword} -> `,longest));
+        const row = i + 3 ;
+        worksheet[`D${row}`] = { v: longest };
+        worksheet[`E${row}`] = { v: shortest };
+
+        XLSX.writeFile(workbook, 'Book1.xlsx'); 
+        await new Promise(resolve => setTimeout(resolve, 1000));
+          browser.close();
+     }
+    }catch (error) {
+      console.log(error)
+     }
     }
-  } catch(e) {
-    console.log(e);
-  }
-})();
-
-
-
+    run()
